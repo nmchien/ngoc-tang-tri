88eda6dc6f4107211f653fb7222eeceafea3604f
nmchien <83529530+nmchien@users.noreply.github.com>
2022-09-20 11:40:04 +0700

    upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Hee luu iêmm!',
+    introDesc: `Chụy có điều này muốn hỏi em, nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'oke',
+    title: 'Em thích chụy có phải không 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là em là ny chụy đó nha :> ',
+    btnYes: 'thích ơi là thích <33',
+    btnNo: 'Chụy mơ à :333',
+    question:'Nói cho chụy lý do vì sao em thích chụy đii <3',
+    btnReply: 'Gửi cho chụy <3',
+    reply: 'Tại vì em đẹp trai, đeo kính kul ngầu hihi <33333333',
+    mess: 'Chụy biết mà 🥰. Love you 300.000 😘😘',
+    messDesc: 'còn giờ thì chờ gì nữa mà không inbox cho chụy đi nào.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/bichngoc0507' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
